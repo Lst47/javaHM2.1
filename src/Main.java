eda6234267ee56b9d54c7081fbe5623d0e96d069
lst47 <linh20121@gmail.com>
2022-07-04 22:23:50 +0300

    2

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13748;
+        int oneMile = 20;
+
+        int bonusMile = price / oneMile;
+
+        System.out.println("Бонусные мили:" + bonusMile );
+    }
+}
